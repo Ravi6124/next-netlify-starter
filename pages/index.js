3e858dab83dbf3a6430a50502858c56ad67876f9
Ravi6124 <36116396+Ravi6124@users.noreply.github.com>
2022-07-06 19:09:47 +0530

    added dev chat_widget

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,7 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+        <script>!function(e,t,a){var c=e.head||e.getElementsByTagName("head")[0],n=e.createElement("script");n.async=!0,n.defer=!0, n.type="text/javascript",n.src=t+"/static/js/chat_widget.js?config="+JSON.stringify(a),c.appendChild(n)}(document,"https://dev.engati.com",{bot_key:"5d94a926815c4210",welcome_msg:true,branding_key:"default",server:"https://dev.engati.com",e:"d" });</script>
       </main>
 
       <Footer />
