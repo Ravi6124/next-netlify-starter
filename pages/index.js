643316f53954f7e77eceec0c2b33ca4d36a16c5e
Ravi6124 <36116396+Ravi6124@users.noreply.github.com>
2022-07-06 19:21:13 +0530

    revert

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-import {Helmet} from "react-helmet";
 
 export default function Home() {
   return (
@@ -16,9 +15,6 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
-        <Helmet>
-        <script>!function(e,t,a){var c=e.head||e.getElementsByTagName("head")[0],n=e.createElement("script");n.async=!0,n.defer=!0, n.type="text/javascript",n.src=t+"/static/js/chat_widget.js?config="+JSON.stringify(a),c.appendChild(n)}(document,"https://dev.engati.com",{bot_key:"5d94a926815c4210",welcome_msg:true,branding_key:"default",server:"https://dev.engati.com",e:"d" });</script>
-        </Helmet>    
   </main>
 
       <Footer />
